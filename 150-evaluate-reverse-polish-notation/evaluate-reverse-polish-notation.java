da603df641567e9d4aef8aa43980d6382ebf7af4
amanpal811-coder <amanpal811@gmail.com>
2025-11-01 11:18:29 +0530

    Time: 6 ms (71.19%) | Memory: 45.3 MB (6.82%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            if (isOperator(token)) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(applyOperator(a, b, token));
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+
+    private boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") ||
+               token.equals("*") || token.equals("/");
+    }
+
+    private int applyOperator(int a, int b, String op) {
+        switch (op) {
+            case "+": return a + b;
+            case "-": return a - b;
+            case "*": return a * b;
+            case "/": return a / b; // Integer division
+            default: throw new IllegalArgumentException("Invalid operator");
+        }
+    }
+}
