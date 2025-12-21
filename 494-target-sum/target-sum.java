2d5e12e423f39e3e76a30ddf027b0ede622d5f16
amanpal811-coder <amanpal811@gmail.com>
2025-12-21 19:07:46 +0530

    Time: 625 ms (19.64%) | Memory: 42.8 MB (86.72%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,22 +1,15 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int total = 0;
-        for(int num : nums) total += num;
-
-        if((total - target) % 2 != 0 || total < target) return 0;
-        int subsetSum = (total-target) / 2;
-        return countSubsets(nums,subsetSum);
+        return dfs(nums, 0, 0, target);
     }
 
-    private int countSubsets(int[] nums,int sum){
-        int[] dp = new int[sum+1];
-        dp[0] = 1;
-
-        for(int num : nums){
-            for(int j = sum; j >= num; j--){
-                dp[j] += dp[j - num];
-            }
+    private int dfs(int[] nums, int index, int sum, int target) {
+        if (index == nums.length) {
+            return sum == target ? 1 : 0;
         }
-        return dp[sum];
+        int add = dfs(nums, index + 1, sum + nums[index], target);
+        int subtract = dfs(nums, index + 1, sum - nums[index], target);
+
+        return add + subtract;
     }
 }
