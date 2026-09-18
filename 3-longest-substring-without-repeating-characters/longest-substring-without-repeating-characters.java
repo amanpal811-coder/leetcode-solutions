293f91b3fcd4cd0fe32a14f5e4ca98b6afaea8c2
Aman Pal <amanpal811@gmail.com>
2026-09-19 01:26:42 +0530

    Time: 70 ms (19.55%) | Memory: 48 MB (21.47%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,15 +3,15 @@ class Solution {
         HashSet<Character> set = new HashSet<>();
         int i = 0;
         int max = 0;
-
         for(int j = 0; j < s.length(); j++){
             while(set.contains(s.charAt(j))){
                 set.remove(s.charAt(i));
                 i++;
             }
             set.add(s.charAt(j));
-            max = Math.max(max, j-i+1);
+        max = Math.max(max,j-i+1);    
         }
+
         return max;
     }
 }
